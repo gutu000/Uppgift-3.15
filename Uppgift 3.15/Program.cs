457e7a79cb5c7a32b2c6b708a7598b69e0c77223
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-30 13:59:16 +0200

    Add project files.

---
+++ Uppgift 3.15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_3._15
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int Tim = int.Parse(Console.ReadLine());
+            int Tot = Tim * 80;
+            
+            if (Tim <= 11)
+            {
+                Console.WriteLine("Okej det kommer att kosta " + Tot + "kr");
+            }
+            else if (Tim >= 11)
+            {
+                Console.WriteLine("Du får tyvär inte hyra bilen, det är för många timmar");
+            }
+
+        
+         
+        }
+    }
+}
